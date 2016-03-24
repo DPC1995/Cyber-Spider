afd1b416b3baff3e9a66e47119cb0388215a9490
DPC1995 <dchambers1995@gmail.com>
2016-03-24 13:23:44 -0700

    Create MultiMapTuple.h

---
+++ MultiMapTuple.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef MULTIMAPTUPLE_H_
+#define MULTIMAPTUPLE_H_
+
+#include <string>
+
+struct MultiMapTuple
+{
+	std::string key;
+	std::string value;
+	std::string context;
+};
+
+#endif // MULTIMAPTUPLE_H_
